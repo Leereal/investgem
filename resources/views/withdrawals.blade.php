2c49dd33f78978c5fa380651d5359dd8386532d6
Leereal <leereal08@ymail.com>
2021-01-06 14:29:41 +0200

    Table Wrap

--- resources/views/withdrawals.blade.php
+++ resources/views/withdrawals.blade.php
@@ -10,7 +10,7 @@
         </div>
         <div class="card-body">
           <div class="table-responsive">
-            <table class="table">
+            <table class="table text-nowrap">
                 <thead class="">
                   <th>DATE</th>
                   <th>USERNAME</th>                  
